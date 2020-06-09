f58c969ccc4bc77b581d52cdb61a2582f830e3c4
internabhijit <intern.abhijit@coutloot.com>
2020-06-10 03:16:40 +0530

    static userroom/chat data

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
+require("./config/mongoConn");
+
+const mode = process.env.NODE_ENV;
+const config =
+  mode == "prod" ? require("./env/production") : require("./env/development");
+const PORT = config.port;
 
 const { addUser, removeUser, getUsers, getUsersInRoom } = require("./users");
-
-const PORT = process.env.PORT || 5000;
-
+const userAuth = require("./routes/userAuth");
 const router = require("./router");
 
 const app = express();
@@ -23,6 +29,7 @@ io.on("connection", (socket) => {
       user: "admin",
       text: `${user.name}, welcome to the room ${user.room}`,
     });
+
     socket.broadcast
       .to(user.room)
       .emit("message", { user: "admin", text: `${user.name}, has joined!` });
@@ -61,7 +68,19 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(router);
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+app.use("/", userAuth);
+app.use(router);
+
+server.listen(PORT, () =>
+  console.log(
+    "Express server started on PORT",
+    PORT,
+    "in",
+    config.mode,
+    "mode."
+  )
+);
